ff1a6e8e9e50894e9f8f070aed12494615293bc6
Rebeca Gaia <reb.gaia@gmail.com>
2021-10-21 17:37:16 -0300

    inicializando

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,38 @@
+import { response } from 'express';
+import axios from "axios";
+/**
+ * Receber code (string)
+ * Recuperar o access_token no github
+ * Recuperar infos do user
+ * Verificar se o usuário existe no DB
+ * ---- SIM = Gera um token
+ * ---- NÃO = Cria no DB, gera um token
+ * Retornar o token com as infos do user
+ */
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        "Accept": "application/json"
+      }
+    });
+
+    const response = await axios.get("");
+
+    return response.data;
+  }
+}
+
+export { AuthenticateUserService } ;
